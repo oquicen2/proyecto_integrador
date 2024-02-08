09dc891bf6f269c08640af195ddba4a7eb22c63d
oquicen2 <141738074+oquicen2@users.noreply.github.com>
2024-02-08 18:49:52 -0500

    CambioOrleis

--- movie/views.py
+++ movie/views.py
@@ -6,9 +6,11 @@ from django.http import HttpResponse
 def home(request):
 #   return HttpResponse('<h1> Welcome to home page</h1>')
 #    return render(request,'home.html')
-    return render(request,'home.html',{'Name': 'greg lim'})
+    return render(request,'home.html',{'Name': 'Orleis Quiceno'})
 
 
 
 def About(request):
     return HttpResponse('<h1> Welcome to About page</h1>')
+
+
