710b390ef146fbe2b8514320c743ad1fb59e5420
oquicen2 <141738074+oquicen2@users.noreply.github.com>
2024-02-09 07:47:38 -0500

    Cambios

--- movie/views.py
+++ movie/views.py
@@ -13,4 +13,4 @@ def home (request):
         return render(request,'home.html',{'searchTerm':searchTerm, 'movies':movies})
 
 def about (request):  
-    return render(request,'about.html')
+        return render(request,'about.html')
