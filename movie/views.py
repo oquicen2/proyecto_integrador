22419baf7edecacb151a9458791771c33c7758cf
oquicen2 <141738074+oquicen2@users.noreply.github.com>
2024-02-08 21:17:19 -0500

    Cear searchMovie

--- movie/views.py
+++ movie/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 
 def home(request):
 #   return HttpResponse('<h1> Welcome to home page</h1>')
 #   return render(request,'home.html')
-    return render(request,'home.html',{'name': 'Orleis Quiceno velasqeuz'})
+#    return render(request,'home.html',{'name': 'Orleis Quiceno velasqeuz'})
 
+    searchTerm = request.GET.get('searchMovie')
+    movie = Movie.objects.all()
+    return render(request,'home.html',{'searchTerm':searchTerm, 'movie':movie})
 
 
 def About(request):
