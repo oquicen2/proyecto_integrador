71d3911e77790472709bb13513d70bdebf111df1
oquicen2 <141738074+oquicen2@users.noreply.github.com>
2024-02-09 07:38:08 -0500

    Views.py

--- movie/views.py
+++ movie/views.py
@@ -4,20 +4,13 @@ from django.http import HttpResponse
 from .models import Movie
 
 # Create your views here.
+def home (request):
+        searchTerm=request.GET.get('searchMovie')
+        if searchTerm:
+                movies = Movie.objects.filter(title__icontains = searchTerm)
+        else:
+                movies = Movie.objects.all()
+        return render(request,'home.html',{'searchTerm':searchTerm, 'movies':movies})
 
-def home(request):
-#   return HttpResponse('<h1> Welcome to home page</h1>')
-#   return render(request,'home.html')
-#    return render(request,'home.html',{'name': 'Orleis Quiceno velasqeuz'})
-
-    searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movie = Movie.objects.filter(title__contains=searchTerm)
-    else:
-        movie = Movie.objects.all()
-    return render(request,'home.html',{'searchTerm':searchTerm, 'movie':movie})
-
-
-def About(request):
-    return HttpResponse('<h1> Welcome to About page</h1</h1>')
-
+def about (request):  
+    return render(request,'about.html')
