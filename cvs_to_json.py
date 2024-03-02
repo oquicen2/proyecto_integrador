4436b6bc4940af218253f113859160e6743cb71d
oquicen2 <141738074+oquicen2@users.noreply.github.com>
2024-03-02 10:05:54 -0500

    cvs_to_json

--- cvs_to_json.py
+++ cvs_to_json.py
@@ -1,22 +1,14 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+import pandas as pd
+import json
 
+df = pd.read_csv('movies_initial.csv')
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moviereviews.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+df.to_json('movies.json', orient='records')
 
+with open('movies.jason', 'r') as file:
+    movies = json.load(file)
 
-if __name__ == '__main__':
-    main()
+for i in range (100):
+    movie = movies[i]
+    print(movie)
+    break
